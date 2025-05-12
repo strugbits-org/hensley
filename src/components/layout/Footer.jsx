821ced538e30fd27cb755b312a642075b615e159
Osyed002 <osyed@strugbitsglobal.com>
2025-05-12 15:28:19 +0500

    header and footer completed with dynamic data

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -5,17 +5,14 @@ import { PrimaryImage } from '../common/PrimaryImage';
 
 export const Footer = ({ data }) => {
   const { footerData, socialLinks, footerNaviationData, branches } = data;
-  console.log("Footer data:", data);
 
   return (
     <footer className='relative footer bg-secondary-alt min-h-screen z-50 flex flex-col justify-between p-6 pt-12'>
-      <div className='footer-content flex flex-wrap justify-between gap-y-6 lg:grow-0 grow'>
-        <div className='order-1 w-full lg:w-1/2 xl:w-3/12'>
-          <h2 className='text-sm font-haasMedium uppercase text-primary mb-[18.5px]'>NewsLetter:</h2>
-          <p className='text-sm font-haasRegular uppercase text-primary mb-5'>REGISTER YOUR EMAIL AND STAY UP TO DATE WITH EVERYTHING BEFORE ANYONE ELSE.</p>
-          <NewsLetter />
+      <div className='footer-content flex justify-between flex-wrap lg:flex-nowrap lg:gap-x-10 gap-y-20 md:gap-y-14 lg:gap-y-6 grow md:grow-0 items-stretch md:items-start'>
+        <div className='order-1 w-full md:w-1/2 lg:w-2/6'>
+          <NewsLetter data={footerData} />
         </div>
-        <div className='order-4 xl:order-2 lg:w-4/12 flex justify-between mb-20'>
+        <div className='order-2 md:order-3 lg:order-2 w-full md:w-2/3 lg:w-2/6 flex justify-between lg:justify-evenly mt-80 md:mt-0'>
           {branches.map((address, index) => (
             <div className='w-2/5 lg:w-1/3' key={index}>
               <h2 className='text-sm font-haasMedium uppercase text-primary mb-1'>{address.title}</h2>
@@ -23,27 +20,26 @@ export const Footer = ({ data }) => {
             </div>
           ))}
         </div>
-        <div className='order-3 w-1/2 lg:w-2/12'>
-          {footerNaviationData.map((item, index) => (
-            <CustomLink to={item.link} key={index} target={item.target}>
-              <p className='text-sm font-haasRegular uppercase text-primary mb-1'>{item.title}</p>
-            </CustomLink>
-          ))}
-          <div className='max-w-[133px] mt-9 flex lg:hidden social-links w-full gap-3 justify-between'>
-            {socialLinks.map((item, index) => (
-              <CustomLink to={item.link} target={item.target} key={index}>
-                <PrimaryImage timeout={0} url={item.icon} defaultDimensions={{ height: 19, width: 19 }} alt="icon" />
+        <div className='order-3 md:order-4 w-1/2 md:w-1/3 lg:w-1/6 flex flex-col md:items-end lg:items-center'>
+          <div className='footer-navigation'>
+            {footerNaviationData.map((item, index) => (
+              <CustomLink to={item.link} key={index} target={item.target}>
+                <p className='text-sm font-haasRegular uppercase text-primary mb-1'>{item.title}</p>
               </CustomLink>
             ))}
+            <div className='max-w-[133px] mt-9 flex lg:hidden social-links w-full gap-3 justify-between'>
+              {socialLinks.map((item, index) => (
+                <CustomLink to={item.link} target={item.target} key={index}>
+                  <PrimaryImage timeout={0} url={item.icon} defaultDimensions={{ height: 19, width: 19 }} alt="icon" />
+                </CustomLink>
+              ))}
+            </div>
           </div>
         </div>
-        <div className='order-2 xl:order-4 w-1/2 xl:w-2/12 flex flex-col items-end'>
-          <div className='max-w-[132px]'>
-
-          </div>
-          <h2 className='text-sm font-haasMedium uppercase text-primary mb-[18.5px]'> {footerData.copyrightText1} </h2>
-          <p className='text-sm font-haasRegular uppercase text-primary mb-5'>{footerData.copyrightText2}</p>
-          <div className='hidden lg:flex social-links w-full gap-3 justify-between'>
+        <div className='order-4 md:order-2 lg:order-4 w-1/2 lg:w-1/6 flex flex-col items-end'>
+          <h2 className='text-end text-sm font-haasMedium uppercase text-primary mb-[18.5px]'> {footerData.copyrightText1} </h2>
+          <p className='text-end text-sm font-haasRegular uppercase text-primary mb-5'>{footerData.copyrightText2}</p>
+          <div className='hidden lg:flex social-links w-full gap-3 justify-between max-w-[133px]'>
             {socialLinks.map((item, index) => (
               <CustomLink to={item.title} key={index} target={item.target}>
                 <PrimaryImage timeout={0} url={item.icon} defaultDimensions={{ height: 19, width: 19 }} alt="icon" />
