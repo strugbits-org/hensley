cf42970f091339f05519c2cfcf170c8e17ff47f1
Osyed002 <osyed@strugbitsglobal.com>
2025-05-21 21:11:01 +0500

    Enhance Listing component to support product banners and improve data fetching logic
    - Added support for product banners in the Listing component.
    - Updated data structure to include categoriesSortData and productBannersData.
    - Refactored fetchSelectedCollectionData to optimize data fetching with Promise.all.
    - Improved ProductBanner component to utilize PrimaryImage and CustomLink.
    - Adjusted ProductCard to include a timeout prop for PrimaryImage.
    - Modified ProductsFilterPopup to use PrimaryImage for the filter icon.
    - Updated MenuFilter styles for better layout.

--- src/components/common/ProductsFilterPopup.jsx
+++ src/components/common/ProductsFilterPopup.jsx
@@ -1,20 +1,16 @@
 import React, { useState } from 'react'
-import catTab from '@/assets/icons/cat-tab.svg'
-import Image from 'next/image';
 import FilterMenu from './MenuFilter';
+import { PrimaryImage } from './PrimaryImage';
 export const ProductsFilterPopup = ({ selectedCategory, subCategories, onFilterChange, selectedFilters }) => {
     const [isActive, setIsActive] = useState(false);
+    const filterIcon = isActive ? "https://static.wixstatic.com/shapes/0e0ac5_afdc5e19d5f849fa973b191220040065.svg" : "https://static.wixstatic.com/shapes/0e0ac5_4aae04aee0b3485b8d8e621d030eecbe.svg";
     return (
-        <div className={`lg:hidden z-10 absolute right-6 top-1/2 translate-y-[-50%]`}>
-            <div className={`absolute right-full top-0 flex z-20 justify-center items-center w-[55px] h-[55px] border border-secondary-alt rounded-[50px]`}>
-                <Image
-                    src={catTab}
-                    onClick={() => setIsActive(!isActive)}
-                    alt='cat-tab'
-                />
+        <div className={`lg:hidden z-10 absolute right-6`}>
+            <div onClick={() => setIsActive(!isActive)} className={`absolute right-full top-0 flex z-20 justify-center items-center w-[55px] h-[55px] border border-secondary-alt rounded-[50px]`}>
+                <PrimaryImage url={filterIcon} alt={"cat-tab"} />
             </div>
             {isActive && (
-                <div className='bg-white absolute -top-4 -right-4 min-h-[300px] w-[330px] rounded-[30px] shadow-lg px-[20px] py-[40px] pt-24'>
+                <div className='bg-white absolute -top-4 -right-4 min-h-[300px] w-[330px] rounded-[30px] shadow-lg px-[20px] py-[40px] pt-16'>
                     <FilterMenu
                         type='popup'
                         selectedCategory={selectedCategory}
