f35e80f51877ccf8039c8c3a0b8d134dd31198e9
Osyed002 <osyed@strugbitsglobal.com>
2025-06-06 15:31:36 +0500

    fix add to cart modal image lsider

--- src/components/Modals/AddToCart/AddToCart.jsx
+++ src/components/Modals/AddToCart/AddToCart.jsx
@@ -88,11 +88,10 @@ const AddToCart = ({ data, onClose }) => {
       setCartQuantity(numValue);
     }
   }, [isProductCollection]);
+
   return (
     <div className='w-[850px] sm:flex-row flex-col flex gap-x-[24px] sm:px-0 px-[20px] bg-primary-alt z-[999999] box-border'>
-      <div className=' h-full sm:max-w-[45%] w-full'>
-        <AddToCartSlider data={data} />
-      </div>
+      <AddToCartSlider data={data} isOpen={data.open} />
       <div className=' h-full sm:w-[55%] w-full py-[20px] pr-[20px] '>
         <div className='w-full flex flex-col gap-y-[15px]'>
           <div className='w-full flex justify-between relative '>
@@ -156,4 +155,4 @@ const AddToCart = ({ data, onClose }) => {
   )
 }
 
-export default AddToCart
+export default AddToCart;
