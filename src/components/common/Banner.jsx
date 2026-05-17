dcb43629c37432a557526aa270095912e2797203
zainulabideen-strugbits <zainulabideen@strugbitsglobal.com>
2026-05-17 15:39:29 +0500

    overlapping and heading issue resolved

--- src/components/common/Banner.jsx
+++ src/components/common/Banner.jsx
@@ -1,30 +1,61 @@
-import React from 'react'
-import { PrimaryImage } from './PrimaryImage';
-import { PrimaryButton } from './PrimaryButton';
-import { CustomLink } from './CustomLink';
+import React from "react";
+import { PrimaryImage } from "./PrimaryImage";
+import { PrimaryButton } from "./PrimaryButton";
+import { CustomLink } from "./CustomLink";
 
 export const Banner = ({ data }) => {
-    if (!data) return null;
-    const { backgroundImage, title, subtitle, buttonLabel, mobileImage, buttonLink } = data;
-    return (
-        <div className='relative banner lg:h-[125vh] h-[100vh] lg:p-6 border border-primary-border'>
-            <div className='absolute lg:inset-6 inset-0 bg-[#babab2]'>
-                <PrimaryImage timeout={0} url={backgroundImage} size="tablet" alt={title} fit='fit' customClasses="hidden lg:block border border-primary-border h-full w-full object-cover" />
-                <PrimaryImage timeout={0} url={mobileImage} size="card" alt={title} fit='fit' customClasses="lg:hidden border border-primary-border h-full w-full object-contain object-[center_250px]" />
-            </div>
-            <div className="absolute inset-0 lg:px-44 py-24 lg:py-20 flex justify-center lg:block">
-                <div className='flex flex-col lg:justify-start justify-between max-w-xs lg:max-w-sm gap-4 lg:gap-6 items-center lg:items-start'>
-                    <span className="text-center lg:block hidden lg:text-start  text-[14px] lg:text-[24px] leading-4 lg:leading-10 font-haasRegular text-white">{subtitle}</span>
-                    <h2 className="text-center lg:block hidden  lg:text-start text-[55px] lg:text-[120px] xl:text-[140px] leading-[55px] lg:leading-[140px] font-recklessRegular text-white">{title}</h2>
-                    <div className='lg:hidden flex flex-col gap-y-[20px]'>
-                        <span className="text-center  lg:text-start block text-[14px] lg:text-[24px] leading-4 lg:leading-10 font-haasRegular text-primary-alt">{subtitle}</span>
-                        <h2 className="text-center block lg:text-start text-[55px] lg:text-[120px] xl:text-[140px] leading-[55px] lg:leading-[140px] font-recklessRegular text-white">{title}</h2>
-                    </div>
-                    <CustomLink to={buttonLink}>
-                        <PrimaryButton className=" font-haasRegular border border-white text-white hover:text-secondary-alt hover:border-secondary-alt text-base hover:bg-primary max-h-[60px] max-w-[280px] px-8 py-4 hover:[letter-spacing:4px]">{buttonLabel}</PrimaryButton>
-                    </CustomLink>
-                </div>
-            </div>
+  if (!data) return null;
+  const {
+    backgroundImage,
+    title,
+    subtitle,
+    buttonLabel,
+    mobileImage,
+    buttonLink,
+  } = data;
+  return (
+    <div className="relative banner lg:h-[125vh] h-[100vh] lg:p-6 border border-primary-border">
+      <div className="absolute lg:inset-6 inset-0 bg-[#babab2]">
+        <PrimaryImage
+          timeout={0}
+          url={backgroundImage}
+          size="tablet"
+          alt={title}
+          fit="fit"
+          customClasses="hidden lg:block border border-primary-border h-full w-full object-cover"
+        />
+        <PrimaryImage
+          timeout={0}
+          url={mobileImage}
+          size="card"
+          alt={title}
+          fit="fit"
+          customClasses="lg:hidden border border-primary-border h-full w-full object-contain object-[center_250px]"
+        />
+      </div>
+      <div className="absolute inset-0 lg:px-16 xl:px-24 2xl:px-44 py-24 lg:py-20 flex justify-center lg:block">
+        <div className="flex flex-col lg:justify-start justify-between max-w-xs lg:max-w-md xl:max-w-xl gap-4 lg:gap-6 items-center lg:items-start">
+          <span className="text-center lg:block hidden lg:text-start  text-[14px] lg:text-[24px] leading-4 lg:leading-10 font-haasRegular text-white">
+            {subtitle}
+          </span>
+          <h2 className="text-center lg:block hidden  lg:text-start text-[55px] lg:text-[100px] xl:text-[130px] 2xl:text-[140px] leading-[55px] lg:leading-[100px] xl:leading-[130px] 2xl:leading-[140px] font-recklessRegular text-white">
+            {title}
+          </h2>
+          <div className="lg:hidden flex flex-col gap-y-[20px]">
+            <span className="text-center  lg:text-start block text-[14px] lg:text-[24px] leading-4 lg:leading-10 font-haasRegular text-primary-alt">
+              {subtitle}
+            </span>
+            <h2 className="text-center block lg:text-start text-[55px] lg:text-[100px] xl:text-[130px] 2xl:text-[140px] leading-[55px] lg:leading-[100px] xl:leading-[130px] 2xl:leading-[140px] font-recklessRegular text-white">
+              {title}
+            </h2>
+          </div>
+          <CustomLink to={buttonLink}>
+            <PrimaryButton className=" font-haasRegular border border-white text-white hover:text-secondary-alt hover:border-secondary-alt text-base hover:bg-primary max-h-[60px] max-w-[280px] px-8 py-4 hover:[letter-spacing:4px]">
+              {buttonLabel}
+            </PrimaryButton>
+          </CustomLink>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
--- src/components/common/TestimonialCard.jsx
+++ src/components/common/TestimonialCard.jsx
@@ -1,21 +1,47 @@
-import React from 'react'
-import { PrimaryImage } from '../common/PrimaryImage';
+import React from "react";
+import { PrimaryImage } from "../common/PrimaryImage";
 
 export const TestimonialCard = ({ data, classes, imageExp, titleClass }) => {
-    const { name, authorName, title, authorTitle, feedback, description, image, avatar } = data;
-    const displayName = authorName || name;
-    const displayTitle = authorTitle || title;
-    const displayImage = avatar || image;
-    return (
-        <div className={`flex flex-col lg:flex-row group  bg-primary lg:bg-transparent relative min-h-[499px] lg:h-full w-full lg:border border-primary-border duration-300 ease-in-out max-w-[1240px] flex-shrink-0 ${classes}`}>
-            <div className={`w-full lg:w-1/2 max-h-[364px] lg:max-h-none h-full relative flex p-3 lg:p-6 ${imageExp ? 'group-hover:lg:p-0' : 'lg:p-6'} transition-all duration-300 ease-in-out`}>
-                <PrimaryImage url={displayImage} size="card" alt={displayTitle} customClasses="grow w-full h-full object-cover" />
-            </div>
-            <div className='w-full lg:w-1/2 p-6 flex flex-col'>
-                <h2 className='text-center lg:text-start text-[35px] leading-[42px] lg:text-[45px] lg:leading-[42px] uppercase tracking-wider text-secondary-alt font-recklessRegular mb-2'>{displayName}</h2>
-                <p className="text-center lg:text-start my-[16px] text-[14px] lg:text-[18px] leading-[30px] text-secondary-alt font-haasRegular uppercase">{feedback || description}</p>
-                <span className={`mt-auto text-center lg:text-start text-[12px] leading-[42px] lg:text-[12px] lg:leading-[42px] uppercase tracking-wider text-secondary-alt font-recklessRegular ${titleClass}`}>{displayTitle}</span>
-            </div>
-        </div>
-    )
-}
+  const {
+    name,
+    authorName,
+    title,
+    authorTitle,
+    feedback,
+    description,
+    image,
+    avatar,
+  } = data;
+  const displayName = authorName || name;
+  const displayTitle = authorTitle || title;
+  const displayImage = avatar || image;
+  return (
+    <div
+      className={`flex flex-col lg:flex-row group  bg-primary lg:bg-transparent relative min-h-[499px] lg:h-full w-full lg:border border-primary-border duration-300 ease-in-out max-w-[1240px] flex-shrink-0 ${classes}`}
+    >
+      <div
+        className={`w-full lg:w-1/2 max-h-[364px] lg:max-h-none h-full relative flex p-3 lg:p-6 ${imageExp ? "group-hover:lg:p-0" : "lg:p-6"} transition-all duration-300 ease-in-out`}
+      >
+        <PrimaryImage
+          url={displayImage}
+          size="card"
+          alt={displayTitle}
+          customClasses="grow w-full h-full object-cover"
+        />
+      </div>
+      <div className="w-full lg:w-1/2 p-6 flex flex-col">
+        <h2 className="text-center lg:text-start text-[35px] leading-[42px] lg:text-[45px] lg:leading-[42px] uppercase tracking-wider text-secondary-alt font-recklessRegular mb-2">
+          {displayName}
+        </h2>
+        <p className="text-center lg:text-start my-[16px] text-[14px] lg:text-[18px] leading-[30px] text-secondary-alt font-haasRegular ">
+          {feedback || description}
+        </p>
+        <span
+          className={`mt-auto text-center lg:text-start text-[12px] leading-[42px] lg:text-[12px] lg:leading-[42px]  tracking-wider text-secondary-alt font-recklessRegular ${titleClass}`}
+        >
+          {displayTitle}
+        </span>
+      </div>
+    </div>
+  );
+};
