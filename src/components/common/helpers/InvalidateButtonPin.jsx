72d419900ca577aa43abb0a0046ca8ffc47d7b1a
osyed002 <osyed@strugbitsglobal.com>
2026-05-23 01:53:20 +0500

    Add SiteScripts component for third-party script integration and update footer data structure

--- src/components/common/helpers/InvalidateButtonPin.jsx
+++ src/components/common/helpers/InvalidateButtonPin.jsx
@@ -45,7 +45,7 @@ export const InvalidateButtonPin = () => {
     return (
         <button 
             onClick={handleClick} 
-            className="min-w-[175px] rotate-90 fixed bottom-36 -right-10 border border-secondary-alt bg-primary p-2 hover:bg-secondary-alt text-secondary-alt hover:text-primary z-[100] transition-all duration-300 uppercase font-haasMedium tracking-widest hover:tracking-[4px] text-base"
+            className="min-w-[175px] rotate-90 fixed bottom-44 -right-10 border border-secondary-alt bg-primary p-2 hover:bg-secondary-alt text-secondary-alt hover:text-primary z-[100] transition-all duration-300 uppercase font-haasMedium tracking-widest hover:tracking-[4px] text-base"
             aria-label="Invalidate"
         >
             Invalidate
