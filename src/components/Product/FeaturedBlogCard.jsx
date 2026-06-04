3f297dfcdf9e96c002780e4551605bcc3d0c1a8b
QaziZunair <qazi@strugbitsglobal.com>
2026-06-04 18:34:39 +0500

    Enhance responsiveness and layout of FeaturedBlogCard, FeaturedBlogs, FeaturedProjects, and Tents components. Update styles to support 3xl screen sizes, improving text sizes, spacing, and button dimensions for better visual presentation and adaptability across devices.

--- src/components/Product/FeaturedBlogCard.jsx
+++ src/components/Product/FeaturedBlogCard.jsx
@@ -9,11 +9,11 @@ function FeaturedBlogCard({ data, classes }) {
     const coverImage = blogRef?.coverImage;
 
     return (
-        <CustomLink to={`/posts/${slug}`} className={`${classes} relative group border border-primary-border pb-2`}>
-            <PrimaryImage alt={title} url={coverImage} size="card" customClasses={"h-full w-full object-cover min-h-[528px] max-h-[528px]"} />
-            <div className='w-full flex gap-1 p-6 pb-0'>
+        <CustomLink to={`/posts/${slug}`} className={`${classes} relative group border border-primary-border pb-2 3xl:pb-4`}>
+            <PrimaryImage alt={title} url={coverImage} size="card" customClasses={"h-full w-full object-cover min-h-[528px] max-h-[528px] 3xl:min-h-[760px] 3xl:max-h-[760px]"} />
+            <div className='w-full flex gap-1 p-6 3xl:p-8 pb-0 3xl:pb-0'>
                 <div className='grow'>
-                    <h2 className="uppercase lg:text-[18px] lg:leading-[20px] text-secondary-alt font-haasRegular mb-3">
+                    <h2 className="uppercase lg:text-[18px] lg:leading-[20px] 3xl:text-[26px] 3xl:leading-[30px] text-secondary-alt font-haasRegular mb-3 3xl:mb-5">
                         {title}
                     </h2>
                 </div>
@@ -21,12 +21,12 @@ function FeaturedBlogCard({ data, classes }) {
                     <PrimaryImage
                         url={"/icons/0e0ac5_87d58241be704c008a2500d6691fb318.png"}
                         alt="Arrow"
-                        customClasses=" h-[20px] group-hover:lg:h-[44px] object-contain transition-all duration-300 ease-in-out"
+                        customClasses=" h-[20px] 3xl:h-[30px] group-hover:lg:h-[44px] object-contain transition-all duration-300 ease-in-out"
                     />
                 </div>
             </div>
-            <div className='px-6'>
-                <p className='text-[12px] leading-[20px] text-secondary-alt font-haasRegular mb-3'>{author?.nickname}</p>
+            <div className='px-6 3xl:px-8'>
+                <p className='text-[12px] leading-[20px] 3xl:text-[18px] 3xl:leading-[26px] text-secondary-alt font-haasRegular mb-3 3xl:mb-5'>{author?.nickname}</p>
 
                 <ul className="flex gap-2 flex-wrap">
                     {markets?.map((market, index) => (
--- src/components/Tents/BannerStructures.jsx
+++ src/components/Tents/BannerStructures.jsx
@@ -16,39 +16,41 @@ const BannerStructures = ({ tent, data = {} }) => {
     const tentName = data.name || tent?.tent?.name || tent?.title || "";
 
     return (
-        <div className='w-full flex flex-col items-center sm:px-0 px-[18px] lg:py-0 py-[48px] justify-between lg:h-[1872px] sm:h-[950px] bg-cover bg-center ' style={{ backgroundImage: `url(${bgUrl})` }}
+        <div className='w-full flex flex-col items-center sm:px-0 px-[18px] lg:py-0 py-[48px] justify-between lg:h-[1872px] 3xl:h-[97.5vw] sm:h-[950px] bg-cover bg-center ' style={{ backgroundImage: `url(${bgUrl})` }}
         >
-            <div className='lg:px-[10px] lg:max-w-[1557px] sm:max-w-[490px] lg:mt-[197px]  w-full flex flex-col justify-center items-center'>
+            <div className='lg:px-[10px] lg:max-w-[1557px] 3xl:max-w-[2400px] sm:max-w-[490px] lg:mt-[197px] 3xl:mt-[280px] w-full flex flex-col justify-center items-center'>
 
                 <div className='w-full flex items-center justify-between'>
                     <h3 className='lg:text-[120px] lg:leading-[55px]
+                    3xl:text-[170px]
+                    3xl:leading-[90px]
                     sm:text-[58px]
                     sm:leading-[70px]
                     text-[45px]
                     leading-[55px]
                     text-white font-recklessRegular uppercase '>{tentName}</h3>
                     <CustomLink to={`/tent/${tentSlug}`} >
-                        <Button text="add to quote" classes={"xl:!height-[130px] xl:!w-[450px] lg:!w-[300px] lg:!h-[130px] lg:block hidden"} />
+                        <Button text="add to quote" classes={"xl:!height-[130px] xl:!w-[450px] lg:!w-[300px] lg:!h-[130px] 3xl:!w-[600px] 3xl:!h-[180px] lg:block hidden"} />
                     </CustomLink>
                 </div>
 
-                <div className='w-full py-[16px] flex lg:flex-row flex-col lg:border-b border-t border-white'>
-                    <div className='lg:w-1/2 flex flex-col gap-y-[24px] lg:mb-0 mb-[16px]'>
+                <div className='w-full py-[16px] 3xl:py-[28px] flex lg:flex-row flex-col lg:border-b border-t border-white'>
+                    <div className='lg:w-1/2 flex flex-col gap-y-[24px] 3xl:gap-y-[40px] lg:mb-0 mb-[16px]'>
 
-                        <span className='text-[16px] leading-[25px] text-white font-haasRegular uppercase block'>
+                        <span className='text-[16px] leading-[25px] 3xl:text-[24px] 3xl:leading-[34px] text-white font-haasRegular uppercase block'>
                             {info}
                         </span>
                     </div>
 
-                    <div className='lg:w-1/2 lg:border-none border-t lg:pt-0 pt-[16px] text-white font-haasRegular uppercase'>
+                    <div className='lg:w-1/2 lg:border-none border-t lg:pt-0 pt-[16px] text-white font-haasRegular uppercase text-[16px] leading-[25px] 3xl:text-[24px] 3xl:leading-[34px]'>
                         {pros}
                     </div>
                 </div>
-                <div className='lg:self-end lg:border-none sm:border-t border-white lg:py-0 py-[16px] lg:mt-[211px] lg:max-w-[292px] w-full lg:gap-y-[110px] gap-y-[16px] flex flex-col'>
-                    <CustomLink to={"/types-of-tents"} className='flex justify-center items-center tracking-[5px] hover:tracking-[8px] transform transition-all duration-300 border border-white h-[45px] lg:w-[292px] w-full text-white uppercase text-[12px] font-haasRegular'>
+                <div className='lg:self-end lg:border-none sm:border-t border-white lg:py-0 py-[16px] lg:mt-[211px] 3xl:mt-[300px] lg:max-w-[292px] 3xl:max-w-[440px] w-full lg:gap-y-[110px] 3xl:gap-y-[150px] gap-y-[16px] flex flex-col'>
+                    <CustomLink to={"/types-of-tents"} className='flex justify-center items-center tracking-[5px] hover:tracking-[8px] transform transition-all duration-300 border border-white h-[45px] 3xl:h-[80px] lg:w-[292px] 3xl:w-[440px] w-full text-white uppercase text-[12px] 3xl:text-[20px] font-haasRegular'>
                         see gallery
                     </CustomLink>
-                    <div className='text-[16px] leading-[25px] text-white font-haasRegular uppercase block'>
+                    <div className='text-[16px] leading-[25px] 3xl:text-[24px] 3xl:leading-[34px] text-white font-haasRegular uppercase block'>
                         {cons}
                     </div>
                 </div>
